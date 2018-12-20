3b8a2c4ab24c26ec77ed1a50b5fcfca9f1907b9d
fotocopia <catalina.pimentel@uniacc.edu>
2018-12-19 21:57:19 -0300

    casi listo, revisar ppt

--- app/src/main/java/com/example/android/clase16/MainActivity.kt
+++ app/src/main/java/com/example/android/clase16/MainActivity.kt
@@ -10,11 +10,42 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.widget.Toast
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 //se necesitau un listener, en este caso LOCATIONLISTENER.
 // Se debe sobreescribir si o si 4 metodos
-class MainActivity : AppCompatActivity(), LocationListener {
+class MainActivity : AppCompatActivity(), LocationListener, OnMapReadyCallback {
+    //var mapa es objeto de la clase
+    var mapa:GoogleMap? = null
+
+    override fun onMapReady(p0: GoogleMap?) {
+        //este val mapa se  creo afuera como metodo de clase porque asi es accesible
+        mapa = p0
+
+        //esto aparece luego de poner ONMAPREADYCALLBACK en la clase
+        //esto significa q cuando el mapa este listo, se haga algo
+        //se debe dar de nuevo los permisos entonces se deben pedir
+      //  p0?.isMyLocationEnabled = true
+        val permisos = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION)
+        var granted = true
+        for (permiso in permisos)
+        {
+            granted = granted and (ActivityCompat.checkSelfPermission(this, permiso) == PackageManager.PERMISSION_GRANTED)
+        }
+        if (!granted)
+        {
+            //aqui se le pone codigo 2 en vez del anterior (el de mas abajo) que dice 1
+            ActivityCompat.requestPermissions(this,permisos,2)
+        }
+        else {
+            p0?.isMyLocationEnabled = true
+        }
+
+
+    }
 
     //necesitamos un locationmanager (asi como los sensormanager)
 
@@ -25,6 +56,10 @@ class MainActivity : AppCompatActivity(), LocationListener {
         setContentView(R.layout.activity_main)
 
         lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        //el fragmento mapa necesita un callback q hremos ahora
+        val fragmentoMapaCallback = supportFragmentManager.findFragmentById(R.id.FragmentMapa) as SupportMapFragment
+        fragmentoMapaCallback.getMapAsync(this)
 
         //el gps es un permiso importante entonces debe salir un popup que le indique al usuario
         //sobre el uso del GPS
@@ -42,6 +77,12 @@ class MainActivity : AppCompatActivity(), LocationListener {
         else {
             lm?.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1f,this)
         }
+
+    //fragmento mapa
+        val fragmentoMapa = supportFragmentManager.findFragmentById(R.id.FragmentMapa) as SupportMapFragment
+        //antes decia THIS con rojo, pro al anadir el listner de mapa (ultimo metodo de main activity_) y
+       // sobreescribir, deja de suceder
+        fragmentoMapa.getMapAsync(this)
     }
 
     //estos son los 4 metodos a implementar obligatoriamente
@@ -86,5 +127,4 @@ class MainActivity : AppCompatActivity(), LocationListener {
     override fun onProviderDisabled(provider: String?) {
         //se borra
     }
-
 }
